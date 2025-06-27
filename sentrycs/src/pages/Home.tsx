71556987d7cbae4a7672b80d3e94442866b2f93d
Yotam Tsamir <tsamir84@gmail.com>
2025-06-27 20:24:46 +0300

    Added listener logic and ui

--- sentrycs/src/pages/Home.tsx
+++ sentrycs/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import LetterList from "../components/LetterList/LetterList";
+import VirtualKeyboard from "../components/VirtualKeyboard/VirtualKeyboard";
 
 const Home = () => {
   return (
     <>
+      <LetterList />
+      <VirtualKeyboard />
     </>
   );
 };
